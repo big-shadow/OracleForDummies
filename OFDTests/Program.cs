98a0183ffcfa2f182e3f34c55ef7ce90450046d5
Ray Winkelman <raywinkelman@gmail.com>
2016-01-26 22:03:22 -0400

    Had to write my own testing framework because of a VS 2015 bug. Restuctured the repo to include it

---
+++ OFDTests/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using OFD;
+
+namespace OFDTests
+{
+    partial class Program
+    {
+        static List<Test> tests = new List<Test>();
+
+        static void Main(string[] args)
+        {
+            MakeTests();
+
+            foreach (Test test in tests)
+            {
+                if (test.RunTest())
+                {
+                    Console.WriteLine("[Yes]   " + test.Name);
+                }
+                else
+                {
+                    Console.WriteLine("[No]    " + test.Name);
+                }
+            }
+
+            Console.Read();
+        }
+    }
+
+    public class Test
+    {
+        public delegate bool Run();
+        public Run RunTest;
+        public string Name;
+
+        public Test(string name, Run func)
+        {
+            this.RunTest = func;
+            this.Name = name;
+        }
+    }
+
+    public class Thing : Model
+    {
+        public string Name { get; set; }
+
+        public Thing(int id = 0) : base(id)
+        {
+
+        }
+    }
+}
---
+++ OFDTests/Tests.cs
@@ -0,0 +1,45 @@
+﻿namespace OFDTests
+{
+    partial class Program
+    {
+        public static void MakeTests()
+        {
+            tests.Add(new Test("Insert Model", delegate
+            {
+                Thing thing = new Thing();
+                thing.Drop();
+
+                thing.Name = "Ray";
+                thing.Save();
+
+                return thing.ID > 0;
+            }));
+
+            tests.Add(new Test("Update Model", delegate
+            {
+                Thing thing = new Thing(1);
+                thing.Name = "Joe";
+                thing.Save();
+
+                return thing.ID == 1;
+            }));
+
+            tests.Add(new Test("Fetch Where ID", delegate
+            {
+                Thing thing = new Thing();
+                thing.Name = "Not Joe";
+                thing.GetWhereID(1);
+
+                return thing.Name.Equals("Joe");
+            }));
+
+            tests.Add(new Test("Constructor Fetch Where ID", delegate
+            {
+                Thing thing = new Thing();
+                thing.GetWhereCondition("Name = 'Joe'");
+
+                return thing.Name.Equals("Joe");
+            }));
+        }
+    }
+}
